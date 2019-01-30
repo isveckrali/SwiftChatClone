37e50eb4f3176d7890f71c8629e2bc3cf8530f82
Mehmet Can Seyhan <mcseyhan4@gmail.com>
2019-01-30 10:10:22 +0300

    Initial Commit

---
+++ FirebaseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Flyco Developer on 30.01.2019.
+//  Copyright © 2019 Flyco Global. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
